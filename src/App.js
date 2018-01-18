7597515ae21ef0aa70dcf2312903759ab6361ba3
ckayra <chiara.pm@gmail.com>
2018-01-19 00:19:02 +0100

    Navbar layout - NOT WORKING

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import React from 'react';
 const App= () => 
 
 <div className="wrapper">
-	<div className="main-head">Header</div>
+	<div className="main-head">
+		<div className="nav-condizioni">COND</div>
+		<div className="nav-menucataweb">CAT</div>
+		<div className="nav-logo">LOGO</div>
+		<div className="nav-agente">AGENTE</div>
+		<div className="nav-cliente">CLIENTE</div>
+		<div className="nav-carrello">CARRELLO</div>
+		<div className="nav-menu">MENU</div>
+		<div className="nav-ricerca">CERCA</div>
+
+
+	</div>
 	<div className="main-leftsidebar">LEFT NAV</div>
 	<div className="main-content">CONTENT</div>
 	<div className="main-footer">FOOTER</div>
-	 
 </div>
 
 export default App;
