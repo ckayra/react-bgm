ca7dc299b8166d64abb8cf25dd81bb6fa362b012
ckayra <chiara.pm@gmail.com>
2018-01-18 20:47:24 +0100

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 
-const App= () => <div>ciao</div>
+const App= () => 
+
+<div className="wrapper">
+	<div className="main-head">Header</div>
+	<div className="main-leftsidebar">LEFT NAV</div>
+	<div className="main-content">CONTENT</div>
+	<div className="main-footer">FOOTER</div>
+	 
+</div>
 
 export default App;
