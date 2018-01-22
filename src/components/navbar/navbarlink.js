f21971778c4c84db5625c8ff3a6b60101cc051d2
ckayra <chiara.pm@gmail.com>
2018-01-22 01:49:30 +0100

    navbar in progress

--- src/components/navbar/navbarlink.js
+++ src/components/navbar/navbarlink.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 const NavBarLink = (props)=>{
 
-  const hasSubMenu=!!props.submenu
-  console.log(hasSubMenu, props)
-
-if (hasSubMenu){
   return( <a href={props.url}>{props.text}</a>)
-} else return(<span>{props.text}</span>)
+
 
 
 }
